a228f20e9494e89d65ec056a413f424aa30e003c
Luis <lgomezm8@miumg.edu.gt>
2020-07-25 01:22:38 -0600

    LUIS: migracion de USER sin problemas

--- jira/models/m_users.py
+++ jira/models/m_users.py
@@ -1,50 +1,50 @@
 from django.db import models
 from jira.models.m_tipo_usuario import TipoUsuario
-from django.contrib.auth.models import AbstractBaseUser , BaseUserManager
+from django.contrib.auth.models import AbstractUser
 
-class MyUserManager(BaseUserManager):
-    def create_user(self,email,username,password=None):
-        if not email:
-            raise ValueError("Users must have an email address")
-        if not username:
-            raise ValueError("Usernames must")
-        user = self.model(
-            email= self.normalize_email(email),
-            username= username
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+# class MyUserManager(BaseUserManager):
+#     def create_user(self,email,username,password=None):
+#         if not email:
+#             raise ValueError("Users must have an email address")
+#         if not username:
+#             raise ValueError("Usernames must")
+#         user = self.model(
+#             email= self.normalize_email(email),
+#             username= username
+#         )
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
     
     
-    def create_superuser(self, email, username, password):
-        user = self.create_user(
-            email= self.normalize_email(email),
-            password=password,
-            username= username
-        )
-        user.save(using=self._db)
-        return user
+#     def create_superuser(self, email, username, password):
+#         user = self.create_user(
+#             email= self.normalize_email(email),
+#             password=password,
+#             username= username
+#         )
+#         user.save(using=self._db)
+#         return user
 
     
-class Account(AbstractBaseUser):
+class Account(AbstractUser):
+
     email=models.EmailField(verbose_name='email', max_length=60, unique=True)
-    username=models.CharField(max_length=30,unique=True)
     first_name=models.CharField(max_length=30)
     last_name=models.CharField(max_length=30)
     tipo = models.ForeignKey('TipoUsuario', on_delete=models.CASCADE)
+
     activo = models.BooleanField(default=True)
     creado = models.DateTimeField(auto_now_add=True)
     modificado = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS =['username','first_name','last_name']
 
     def __str__(self):
-        return self.email+ " "+ self.username
+        """Return username."""
+        return self.email
 
-    def has_perm(self, perm,obj):
-        return self.tipo
-        
-    def has_module_perms(self,app_label):
-        return True
+    def get_short_name(self):
+        """Return username."""
+        return self.email
--- jira/views/v_users.py
+++ jira/views/v_users.py
@@ -1,8 +1,9 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
 from rest_framework import status, filters, viewsets
-
+from rest_framework.response import Response
 from jira.serializers import s_users
+from jira.serializers.s_users import UserSerializer
 from jira.models.m_users import Account
 
 
@@ -17,3 +18,28 @@ class UsuariosViewSet(viewsets.ModelViewSet):
 
     queryset = Account.objects.all()
     serializer_class = s_users.UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        usuario = Account.objects.get(username=request.data["username"])
+        usuario.set_password(request.data["password"])
+        usuario.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    @action(detail=False, methods=['post'])
+    #signup es la etiqueta de la url asi lo redirigue asia aca... 
+    def login(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        data = {
+            'user': s_users.UserSerializer(user).data,
+            'access_token': 'token'
+        }
+        return Response(data, status=status.HTTP_201_CREATED)    
